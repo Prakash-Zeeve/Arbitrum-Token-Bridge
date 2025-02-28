b782d06246af965c359b8161c128d5b130b26619
mdtosif <md.tosif@zeeve.io>
2025-02-28 16:38:13 +0530

    zeeve logo change

--- packages/arb-token-bridge-ui/src/components/common/Header.tsx
+++ packages/arb-token-bridge-ui/src/components/common/Header.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Image from 'next/image'
 import { twMerge } from 'tailwind-merge'
-import ZkasinoLogo from '@/images/zkasinoLogo.png'
+import Logo from '@/images/zeeve-logo.png'
 
 import { isNetwork } from '../../util/networks'
 import { useNetworks } from '../../hooks/useNetworks'
@@ -21,11 +21,7 @@ export function Header({ children }: { children?: React.ReactNode }) {
       )}
     >
       <div className="flex w-full items-center justify-end gap-2 text-white">
-        <Image
-          className="mr-auto aspect-[813/316] w-20"
-          src={ZkasinoLogo}
-          alt="Zkasino"
-        />
+        <Image className="mr-auto w-10" src={Logo} alt="Zeeve Bridge" />
         {isTestnet && <span className="grow font-medium">TESTNET MODE</span>}
         <div className="hidden sm:flex">{children}</div>
       </div>
